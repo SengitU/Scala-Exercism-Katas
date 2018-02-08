cdb50a259be3e29f543f5e1939f832fbaa53e658
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-02-08 17:10:34 +0100

    Improved the performance

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,17 +1,49 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = {
-    var currentNumber: Int = 0
-    var sumOfAll = 0
+  /*
+   *
+   * #ofIterations is limit - 1
+   *
+   */
+  // def sum(factors: Set[Int], limit: Int): Int = {
+  //   var currentNumber: Int = 0
+  //   var sumOfAll = 0
+  //   var numOfIterations = 0;
 
-    for(currentNumber <- 1 until limit) {
-      val isMultiplier = factors.find((factor: Int) => (currentNumber % factor) == 0)
+  //   for(currentNumber <- 1 until limit) {
+  //     val isMultiplier = factors.find((factor: Int) => (currentNumber % factor) == 0)
       
-      if(isMultiplier ne None) {
-        sumOfAll += currentNumber
-      }
-    }
+  //     if(isMultiplier ne None) {
+  //       sumOfAll += currentNumber
+  //     }
+  //     numOfIterations += 1
+  //   }
+  //   println(numOfIterations)
+  //   return sumOfAll
+  // }
 
-    return sumOfAll
+  /*
+   * #ofIterations 
+   * Increases by number of factors
+   * Bigger factor number yields to lesser iterations
+   * Limit increase usually increases iterations but not as much as the other one
+   */
+  def sum(factors: Set[Int], limit: Int): Int = {
+    var multipliers: Set[Int] = Set(0)
+    var numOfIterations = 0;
+
+    factors.foreach((factor) => {
+      var currentNumber: Int = 0
+      for(currentNumber <- 1 to Math.floor(limit / factor).asInstanceOf[Int]) {
+        val multiplier = currentNumber * factor
+        if(multiplier != limit) {
+          multipliers += multiplier
+        }
+        numOfIterations += 1
+      }
+    })
+
+    println(numOfIterations + multipliers.size)
+    return multipliers.reduceLeft(_ + _)
   }
 }
 
